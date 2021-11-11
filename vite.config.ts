87d1fcde07b57308d5501591e60fcc9c0f96d622
Rigoberto Moya González <rigomoya2@gmail.com>
2021-11-11 12:49:46 -0500

    Setup library build files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,47 @@
+import * as path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/lib/index.ts'),
+      name: 'ServerDataGrid',
+      fileName: (format) => `server-data-grid.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: [
+        "@apollo/client",
+        "@emotion/react",
+        "@emotion/styled",
+        "@mui/lab",
+        "@mui/material",
+        "@mui/x-data-grid",
+        "@tabler/icons",
+        "graphql",
+        "lodash",
+        "notistack",
+        "pluralize",
+        "react",
+        "react-dom",
+        "react-hook-form",
+        "react-router-dom",
+        "sentence-case",
+        "title-case",
+      ],
+      output: {
+        globals: {
+          react: 'React',
+          '@mui/material': 'MaterialUI',
+          '@apollo/client': 'ApolloClient',
+          '@mui/lab': 'MaterialUILab'
+        }
+      }
+    }
+  }
 })
